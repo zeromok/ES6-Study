7d1ab457326b6b9226357e75c171bfb0f7e4ced6
Daniel <byoung.mokk@gmail.com>
2022-11-11 14:02:34 +0900

    Object Destructuring

--- destructuring/example00.js
+++ destructuring/example00.js
@@ -68,3 +68,59 @@ console.log([year, month, day]);
 // =====================================
 // Object Destructuring(객체 디스트럭처링)
 // =====================================
+
+// ES5
+var obj00 = { firstName00: 'Ungmo', lastName00: 'Lee' };
+
+var firstName00 = obj00.firstName00;  // Ungmo
+var lastname00 = obj00.lastName00;    // Lee
+
+// ES6
+let obj = { firstName: 'Ungmo', lastName: 'Lee' };
+
+/*
+    프로퍼티 키 기준으로 디스트럭처링 할당이 이루어진다. 순서는 의미 없다.
+    변수 lastName, firstName 가 선언되고 obj가 디스트럭처링 되어 할당된다.
+*/
+let {lastName, firstName} = obj;
+console.log(lastName);  // Lee
+console.log(firstName); // Ungmo
+
+/*
+    프로퍼티 키가 prop1인 프로퍼티의 값을 변수 p1에 할당
+    프로퍼티 키가 prop2인 프로퍼티의 값을 변수 p2에 할당
+    const { prop1: p1, prop2: p2 } = { prop1: 'a', prop2: 'b' };
+    console.log(p1, p2); // 'a' 'b'
+    console.log({ prop1: p1, prop2: p2 }); // { prop1: 'a', prop2: 'b' }
+
+    아래는 위의 축약형이다
+    const { prop1, prop2 } = { prop1: 'a', prop2: 'b' };
+    console.log({ prop1, prop2 }); // { prop1: 'a', prop2: 'b' }
+
+    default value
+    const { prop1, prop2, prop3 = 'c' } = { prop1: 'a', prop2: 'b' };
+    console.log({ prop1, prop2, prop3 }); // { prop1: 'a', prop2: 'b', prop3: 'c' }
+*/
+
+// 객체에서 프로퍼티 키 로 필요한 프로퍼티 값만 추출하기
+const todos = [
+    {
+               id: 1,
+          content: 'HTML',
+        completed: true 
+    },
+    {
+               id: 2,
+          content: 'CSS',
+        completed: false
+    },
+    {
+               id: 3,
+          content: 'JS',
+        completed: false
+    }
+  ];
+  
+  // todos 배열의 요소인 객체로부터 completed 프로퍼티만을 추출한다.
+  const completedTodos = todos.filter(({ completed }) => completed);
+  console.log(completedTodos); // [ { id: 1, content: 'HTML', completed: true } ]
