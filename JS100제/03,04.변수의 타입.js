d6d1fcad94f69798bd6507801a50dc3bd4723315
Daniel <byoung.mokk@gmail.com>
2022-11-16 14:34:23 +0900

    Update

---
+++ JS100제/03,04.변수의 타입.js
@@ -0,0 +1,25 @@
+/*
+    문제 3 : 변수의 타입
+
+    다음 출력 값으로 올바른 것은?
+*/
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+//답 : object
+
+
+/*
+    문제 4 : 변수의 타입2
+
+    다음 변수 a를 typeof(a)로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+
+    1)  입력 : a =1,   출력 : number
+    2)  입력 : a = 2.22,   출력 : boolean
+    3)  입력 : a = 'p',   출력 : string
+    4)  입력 : a = [1, 2, 3],   출력 : object
+*/
+let a = 2.22;
+console.log(typeof(a));
+
+// 답 : 2(Number)
