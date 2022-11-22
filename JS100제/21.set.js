09c732b1c54128f1cdfaa5e61b3d1cdcb35e8569
zeromok <byoung.mokk@gmail.com>
2022-11-23 08:32:24 +0900

    21.Set Update

---
+++ JS100제/21.set.js
@@ -0,0 +1,13 @@
+/*
+    문제 21. : set은 어떻게 만드나요?
+
+    다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+    1)  var x = {1, 2, 3, 5, 6, 7};     -> 객체
+    2)  var x = {};                     -> 객체
+    3)  var x = new Set('javascript');  -> 올바른 set 선언
+    4)  var x = new Set(range(5));      -> undefined
+    5)  var x = new Set();              -> 올바른 set 선언
+
+    3, 5번
+*/
