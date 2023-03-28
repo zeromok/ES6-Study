7b5a0f37a201a51de9e10a0e6f3dcaebea6745ec
Daniel <byoung.mokk@gmail.com>
2023-03-28 23:58:09 +0900

    Study: 61.문자열 압축하기.js

---
+++ JS100제(51~75)/61.문자열 압축하기.js
@@ -0,0 +1,27 @@
+/*
+    # 문제61 : 문자열 압축하기
+
+    문자열을 입력받고 연속되는 문자열을 압축해서 표현하고 싶습니다.
+
+    입력
+    aaabbbbcdddd
+
+    출력
+    a3b4c1d4
+*/
+// 1.
+(function (str) {
+    let count = 0;
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        if(str.charAt(i) === str.charAt(i+1)){
+            count++;
+        }else {
+            result += str.charAt(i) + (count + 1);
+            count = 0;
+        }
+    }
+    console.log(result);
+})('aaabbbbcdddd')
+
+
