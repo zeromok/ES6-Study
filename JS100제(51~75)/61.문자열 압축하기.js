bab1423b57aca424fd4a572274b87a1f5a5f9f50
Daniel <byoung.mokk@gmail.com>
2023-03-29 08:34:58 +0900

    Study: 61.문자열 압축하기.js

--- JS100제(51~75)/61.문자열 압축하기.js
+++ JS100제(51~75)/61.문자열 압축하기.js
@@ -10,18 +10,34 @@
     a3b4c1d4
 */
 // 1.
+// (function (str) {
+//     let count = 0;
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         if(str.charAt(i) === str.charAt(i+1)){
+//             count++;
+//         }else {
+//             result += str.charAt(i) + (count + 1);
+//             count = 0;
+//         }
+//     }
+//     console.log(result);
+// })('aaabbbbcdddd')
+
+
+// 2.
 (function (str) {
-    let count = 0;
     let result = '';
-    for (let i = 0; i < str.length; i++) {
-        if(str.charAt(i) === str.charAt(i+1)){
-            count++;
-        }else {
-            result += str.charAt(i) + (count + 1);
-            count = 0;
-        }
-    }
+    let count = 0;
+   str.split("")
+       .map((item, idx, arr) => {
+           if (item === arr[idx + 1]) {
+               count++;
+           } else {
+               result = result.concat(item + (count + 1));
+               count = 0;
+           }
+       });
     console.log(result);
 })('aaabbbbcdddd')
-
 
